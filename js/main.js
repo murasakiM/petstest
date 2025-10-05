2965110c2942765e16ffec21dde75addd1041d48
murasakiM <61902443+murasakiM@users.noreply.github.com>
2025-10-05 12:00:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
